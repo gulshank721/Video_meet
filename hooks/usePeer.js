816e022354338b1df0cc3e45abc564731040cc94
Gulshan Kumar <gulshank721@gmail.com>
2024-07-25 10:18:43 +0530

    copy roomId added

--- hooks/usePeer.js
+++ hooks/usePeer.js
@@ -2,8 +2,10 @@ import React, { useEffect, useRef, useState } from "react";
 import Peer from "peerjs";
 import { useSocket } from "@/context/socket";
 import { useParams } from "next/navigation";
+import { useRouter } from "next/router";
 
 export const usePeer = () => {
+  const router = useRouter();
   const socket = useSocket();
   const [peer, setPeer] = useState(null);
   const [myId, setMyId] = useState("");
